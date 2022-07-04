0ffc1fb396a37ad0ad2846b08280a2efd45e401e
vasokaloutsa <v.kaloutsa@windowslive.com>
2022-07-04 20:17:55 +0300

    feat: add infinite scrolling & restyle cards

--- src/components/ComicCard/index.jsx
+++ src/components/ComicCard/index.jsx
@@ -1,23 +1,17 @@
 import "./styles.css";
 
 export default function ComicCard({ data }) {
-  console.log(data);
   return (
     <div className="wrapper">
-      {data.images[0] ? (
-        <img
-          src={`${data.images[0]?.path}/standard_large.${data.images[0]?.extension}`}
-          alt="comic"
-        />
-      ) : (
-        <p>No image found</p>
-      )}
-      <p>{data.title}</p>
-      <p className="description">
-        {data.description ? data.description : "No description provided"}
-      </p>
+      <img
+        src={`${data.images[0]?.path}/standard_large.${data.images[0]?.extension}`}
+        alt="comic"
+        className="comicImg"
+      />
+
+      <p className="title">{data.title}</p>
       <p>Issue Number: {data.issueNumber}</p>
-      <p>Price: {data.prices[0].price} $</p>
+      <p>Price: {data.prices[0].price ? data.prices[0].price : "-"} $</p>
     </div>
   );
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,19 +1,42 @@
 import { useEffect, useState } from "react";
 import ComicCard from "../ComicCard";
+import "./styles.css";
 
-
+let offset = 0;
 export default function Home() {
   const [data, setData] = useState([]);
-  const CHARACTERS_URL = `http://gateway.marvel.com/v1/public/comics?ts=1&apikey=c73b8ffc61ed4a88fc2672e4dcca239e&hash=84be1de7f4d348318c62104656b00587`;
-  useEffect(() => {
-    fetch(CHARACTERS_URL)
+
+  const fetchData = () => {
+    console.log(offset);
+    fetch(
+      `http://gateway.marvel.com/v1/public/comics?ts=1&apikey=c73b8ffc61ed4a88fc2672e4dcca239e&hash=84be1de7f4d348318c62104656b00587&offset=${offset}`
+    )
       .then((res) => res.json())
-      .then((data) => setData(data.data.results));
-  }, [CHARACTERS_URL]);
+      .then((data) => {
+        const newData = [];
+        data.data.results.forEach((p) => newData.push(p));
+        setData((oldData) => [...oldData, ...newData]);
+      });
+    offset += 20;
+  };
+
+
+  const handleScroll = () => {
+    let userScrollHeight = window.innerHeight + window.scrollY;
+    let windowBottomHeight = document.documentElement.offsetHeight;
+    if (userScrollHeight >= windowBottomHeight) {
+      fetchData();
+    }
+  };
+  useEffect(() => {
+    fetchData();
+    window.addEventListener("scroll", handleScroll); // attaching scroll event listener
+  },[]);
+
   return (
-    <div>
-      {data.map((comic) => (
-        <ComicCard data={comic} key={comic.id}/>
+    <div className="wrapper-grid">
+      {data.map((comic, idx) => (
+        <ComicCard data={comic} key={idx} />
       ))}
     </div>
   );
